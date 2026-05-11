db6fd4bb4ecd30f6992c287f65cd870a12efba8b
DeveloperManojKumarMeena <manojkumarmeena1996@outlook.com>
2026-05-11 17:11:42 +0530

    product: add model, route and connect to app.js

--- backend/product/src/app.js
+++ backend/product/src/app.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
+const router = require('./routes/product.route')
 
 const express = require('express');
 
 const app = express();
 app.use(express.json());
+app.use("api/products",router);
 
 
 module.exports = app;
---
+++ backend/product/src/model/product.model.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    price:{
+        amount:{
+            type:Number,
+            required:true   },
+        currency:{
+            type:String,
+            required:true,
+            enum:["USD","INR"]
+        }
+    },
+    category:{
+        type:String,
+        required:true
+    },
+    seller:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    images:{
+        url:{
+            type:String,
+            required:true
+        },
+        thumbnailUrl:{
+            type:String,
+            required:true
+        }
+    }
+    
+})
+
+const ProductModel = mongoose.model("Product",productSchema);
+
+module.exports = ProductModel;
